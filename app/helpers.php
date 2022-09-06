b8c53ee36d68abe1f74dbacbb942a01f96dc3583
harbyanwardi <anwardiharby@gmail.com>
2022-09-06 17:17:05 +0700

    1st

---
+++ app/helpers.php
@@ -0,0 +1,22 @@
+<?php 
+
+use Illuminate\Support\Facades\Http;
+
+function createPremiumAccess($data)
+{
+    $url = env('SERVICE_COURSE_URL').'api/mycourse/premium';
+    try {
+        $response = Http::post($url, $data);
+        $data = $response->json();
+        $data['http_code'] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            'status' => 'error',
+            'http_code' => 500,
+            'message' => 'Service Course Unavailable'
+        ];
+    }
+}
+
+?>
